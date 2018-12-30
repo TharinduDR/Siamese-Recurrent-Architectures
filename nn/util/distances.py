642648930b7a977b26d473e1a8cd438fa60c9a70
TharinduDR <rhtdranasinghe@gmail.com>
2018-12-30 10:02:55 +0000

    003: Added GRU model

---
+++ nn/util/distances.py
@@ -0,0 +1,6 @@
+import keras.backend as K
+
+
+def exponent_neg_manhattan_distance(left, right):
+    ''' Helper function for the similarity estimate of the LSTMs outputs'''
+    return K.exp(-K.sum(K.abs(left - right), axis=1, keepdims=True))
--- utility/run_experiment.py
+++ utility/run_experiment.py
@@ -1,9 +1,6 @@
-from models.sentence import Sentence
-
-
 def run_experiment(df, benchmark):
-    sentences1 = [Sentence(s) for s in df['sent_1']]
-    sentences2 = [Sentence(s) for s in df['sent_2']]
+    sentences1 = df['sent_1']
+    sentences2 = df['sent_2']
 
     sims = benchmark[1](sentences1, sentences2)
 

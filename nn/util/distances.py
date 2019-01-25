42b61ce61540e87d84e9ac887597d6981c1a7151
TharinduDR <rhtdranasinghe@gmail.com>
2019-01-25 11:20:31 +0000

    019: Changing the file names

--- nn/util/distances.py
+++ nn/util/distances.py
@@ -4,3 +4,7 @@ import keras.backend as K
 def exponent_neg_manhattan_distance(left, right):
     """ Helper function for the similarity estimate of the LSTMs outputs"""
     return K.exp(-K.sum(K.abs(left - right), axis=1, keepdims=True))
+
+def exponent_neg_euclidian_distance(left, right):
+    """ Helper function for the similarity estimate of the LSTMs outputs"""
+    return K.exp(-K.sum(K.abs(left - right), axis=1, keepdims=True))

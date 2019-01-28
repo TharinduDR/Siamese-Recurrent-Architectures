d01ccfd781739e29f9eaa671c76939bbc5255e5f
TharinduDR <rhtdranasinghe@gmail.com>
2019-01-28 15:21:52 +0000

    019: updating distances

--- nn/util/distances.py
+++ nn/util/distances.py
@@ -8,4 +8,4 @@ def exponent_neg_manhattan_distance(left, right):
 
 def exponent_neg_euclidean_distance(left, right):
     """ Helper function for the similarity estimate of the LSTMs outputs"""
-    return K.exp(-K.sqrt(K.sum(K.square(left - right), axis=-1, keepdims=True)))
+    return K.exp(-K.sqrt(K.sum(K.square(K.abs(left - right)), axis=1, keepdims=True)))

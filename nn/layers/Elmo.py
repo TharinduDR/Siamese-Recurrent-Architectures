a781a86a9568686d6eb82162d2e03e5699b04db6
TharinduDR <rhtdranasinghe@gmail.com>
2019-01-30 15:01:16 +0000

    019: Adding embedding layer for ELMo

---
+++ nn/layers/Elmo.py
@@ -0,0 +1,30 @@
+import keras.backend as K
+import tensorflow as tf
+import tensorflow_hub as hub
+from keras.engine import Layer
+
+
+class ElmoEmbeddingLayer(Layer):
+    def __init__(self, **kwargs):
+        self.elmo = hub.Module('https://tfhub.dev/google/elmo/2', trainable=self.trainable,
+                               name="{}_module".format(self.name))
+        self.dimensions = 1024
+        self.trainable = True
+        super(ElmoEmbeddingLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.trainable_weights += K.tf.trainable_variables(scope="^{}_module/.*".format(self.name))
+        super(ElmoEmbeddingLayer, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        result = self.elmo(K.squeeze(K.cast(x, tf.string), axis=1),
+                           as_dict=True,
+                           signature='default',
+                           )['default']
+        return result
+
+    def compute_mask(self, inputs, mask=None):
+        return K.not_equal(inputs, '--PAD--')
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], self.dimensions

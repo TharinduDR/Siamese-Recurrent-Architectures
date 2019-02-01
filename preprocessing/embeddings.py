61c755dd18c695eae34fa5e620c370dd179fbdad
TharinduDR <rhtdranasinghe@gmail.com>
2019-02-01 09:37:13 +0000

    019: Combining embeddings

--- preprocessing/embeddings.py
+++ preprocessing/embeddings.py
@@ -42,3 +42,47 @@ def prepare_embeddings(model, datasets, question_cols):
             embeddings[index] = model.word_vec(word)
 
     return embeddings
+
+
+def merge_embeddings(models, datasets, question_cols):
+    vocabulary = dict()
+    inverse_vocabulary = ['<unk>']
+    questions_cols = question_cols
+
+    # Iterate over the questions only of both training and test datasets
+    for dataset in datasets:
+        for index, row in dataset.iterrows():
+
+            # Iterate through the text of both questions of the row
+            for question in questions_cols:
+
+                q2n = []  # q2n -> question numbers representation
+                for word in text_to_word_list(row[question]):
+
+                    word_exists = True
+                    for model in models:
+                        if word not in model.vocab:
+                            word_exists = False
+                    if not word_exists:
+                        continue
+
+                    if word not in vocabulary:
+                        vocabulary[word] = len(inverse_vocabulary)
+                        q2n.append(len(inverse_vocabulary))
+                        inverse_vocabulary.append(word)
+                    else:
+                        q2n.append(vocabulary[word])
+
+                # Replace questions as word to question as number representationindex, question, q2n
+                dataset.set_value(index, question, q2n)
+
+    embedding_dim = 300
+    embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)  # This will be the embedding matrix
+    embeddings[0] = 0  # So that the padding will be ignored
+
+    # Build the embedding matrix
+    for word, index in vocabulary.items():
+        if word in model.vocab:
+            embeddings[index] = model.word_vec(word)
+
+    return embeddings

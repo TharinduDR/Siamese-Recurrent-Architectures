852c765b39f08624cc1fbf4a658499dec41a603d
TharinduDR <rhtdranasinghe@gmail.com>
2019-02-01 09:36:33 +0000

    019: Trying out ELMo embedding layer

--- nn/layers/Elmo.py
+++ nn/layers/Elmo.py
@@ -2,17 +2,21 @@ import keras.backend as K
 import tensorflow as tf
 import tensorflow_hub as hub
 from keras.engine import Layer
+from keras.utils.generic_utils import to_list
 
 
 class ElmoEmbeddingLayer(Layer):
-    def __init__(self, **kwargs):
-        self.elmo = hub.Module('https://tfhub.dev/google/elmo/2', trainable=self.trainable,
-                               name="{}_module".format(self.name))
-        self.dimensions = 1024
+    def __init__(self, output_dim, input_length=None, mask_zero=False, **kwargs, ):
+
+        self.output_dim = output_dim
+        self.input_length = input_length
+        self.mask_zero = mask_zero
         self.trainable = True
-        super(ElmoEmbeddingLayer, self).__init__(**kwargs)
+        super(ElmoEmbeddingLayer, self).__init__(trainable=self.trainable, **kwargs)
 
     def build(self, input_shape):
+        self.elmo = hub.Module('https://tfhub.dev/google/elmo/2', trainable=self.trainable,
+                               name="{}_module".format(self.name))
         self.trainable_weights += K.tf.trainable_variables(scope="^{}_module/.*".format(self.name))
         super(ElmoEmbeddingLayer, self).build(input_shape)
 
@@ -24,7 +28,27 @@ class ElmoEmbeddingLayer(Layer):
         return result
 
     def compute_mask(self, inputs, mask=None):
-        return K.not_equal(inputs, '--PAD--')
+        if not self.mask_zero:
+            return None
+        output_mask = K.not_equal(inputs, 0)
+        return output_mask
 
     def compute_output_shape(self, input_shape):
-        return input_shape[0], self.dimensions
+        if self.input_length is None:
+            return input_shape + (self.output_dim,)
+        else:
+            # input_length can be tuple if input is 3D or higher
+            in_lens = to_list(self.input_length, allow_tuple=True)
+            if len(in_lens) != len(input_shape) - 1:
+                raise ValueError(
+                    '"input_length" is %s, but received input has shape %s' %
+                    (str(self.input_length), str(input_shape)))
+            else:
+                for i, (s1, s2) in enumerate(zip(in_lens, input_shape[1:])):
+                    if s1 is not None and s2 is not None and s1 != s2:
+                        raise ValueError(
+                            '"input_length" is %s, but received input has shape %s' %
+                            (str(self.input_length), str(input_shape)))
+                    elif s1 is None:
+                        in_lens[i] = s2
+            return (input_shape[0],) + tuple(in_lens) + (self.output_dim,)

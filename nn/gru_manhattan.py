8362ebadc4ec9c109a9f889e5c3a711071afbe1a
TharinduDR <rhtdranasinghe@gmail.com>
2019-02-01 10:24:00 +0000

    019: Adding multiple vector models

--- nn/gru_manhattan.py
+++ nn/gru_manhattan.py
@@ -6,14 +6,14 @@ from keras_preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 
 from nn.util.distances import exponent_neg_manhattan_distance
-from preprocessing.embeddings import prepare_embeddings
+from preprocessing.embeddings import merge_embeddings
 
 
-def run_gru_benchmark(train_df, test_df, sent_cols, sim_col, validation_portion=0.1, n_hidden=100, embedding_dim=300,
+def run_gru_benchmark(train_df, test_df, sent_cols, sim_col, validation_portion=0.1, n_hidden=100,
                       batch_size=64, n_epoch=500, optimizer=None, save_weights=None, load_weights=None,
-                      max_seq_length=None, model=None):
+                      max_seq_length=None, models=None):
     datasets = [train_df, test_df]
-    embeddings = prepare_embeddings(datasets=datasets, question_cols=sent_cols, model=model)
+    embeddings, embedding_dim = merge_embeddings(datasets=datasets, question_cols=sent_cols, models=models)
 
     if max_seq_length is None:
         max_seq_length = max(train_df.sent_1.map(lambda x: len(x)).max(),

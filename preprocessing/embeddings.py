e9d44676da656e06bf011c69371de02611d131a1
TharinduDR <rhtdranasinghe@gmail.com>
2019-02-01 15:36:13 +0000

    019: Adding merge embeddings

--- preprocessing/embeddings.py
+++ preprocessing/embeddings.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import numpy as np
 
 from preprocessing.cleaning import text_to_word_list
@@ -46,7 +48,7 @@ def prepare_embeddings(model, datasets, question_cols):
     return embeddings
 
 
-def merge_embeddings(models, datasets, question_cols):
+def merge_embeddings(models, datasets, question_cols, merge_operation):
     vocabulary = dict()
     inverse_vocabulary = ['<unk>']
     questions_cols = question_cols
@@ -75,24 +77,48 @@ def merge_embeddings(models, datasets, question_cols):
                     else:
                         q2n.append(vocabulary[word])
 
-                # Replace questions as word to question as number representationindex, question, q2n
+                # Replace questions as word to question as number representation index, question, q2n
                 dataset.set_value(index, question, q2n)
 
     embedding_dim = 0
-    for model in models:
-        embedding_dim = embedding_dim + model.vector_size
+
+    if merge_operation is MergeOperations.CONCATENATE:
+        for model in models:
+            embedding_dim = embedding_dim + model.vector_size
+
+    if merge_operation is MergeOperations.AVERAGE:
+        embedding_dim = models[0].vector_size
+
     embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)  # This will be the embedding matrix
     embeddings[0] = 0  # So that the padding will be ignored
 
     # Build the embedding matrix
-    for word, index in vocabulary.items():
 
-        counter = 0
-        for model in models:
-            if word in model.vocab:
-                if counter == 0:
-                    embeddings[index] = model.word_vec(word)
-                else:
-                    embeddings[index] = np.concatenate((embeddings[index], model.word_vec(word)), axis=0)
-            counter += 1
-    return embeddings, embedding_dim
+    if merge_operation is MergeOperations.CONCATENATE:
+        for word, index in vocabulary.items():
+            counter = 0
+            temp_embedding = []
+            for model in models:
+                if word in model.vocab:
+                    temp_embedding = temp_embedding + model.word_vec(word).tolist()
+
+                counter += 1
+            embeddings[index] = np.asarray(temp_embedding, dtype=np.float32)
+        return embeddings, embedding_dim
+
+    if merge_operation is MergeOperations.AVERAGE:
+        for word, index in vocabulary.items():
+            counter = 0
+            temp_embedding = []
+            for model in models:
+                if word in model.vocab:
+                    temp_embedding.append(model.word_vec(word).tolist())
+
+                counter += 1
+            embeddings[index] = np.asarray(np.mean(temp_embedding, axis=0), dtype=np.float32)
+        return embeddings, embedding_dim
+
+
+class MergeOperations(Enum):
+    AVERAGE = 'average'
+    CONCATENATE = 'concatenate'

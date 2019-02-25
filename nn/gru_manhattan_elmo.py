35f6033ab38a3b303027082946d2d429154eb2d1
TharinduDR <rhtdranasinghe@gmail.com>
2019-02-25 10:45:51 +0000

    019: Adding elmo model

--- nn/gru_manhattan_elmo.py
+++ nn/gru_manhattan_elmo.py
@@ -1,19 +1,22 @@
 import itertools
 
 from keras import Input, Model
-from keras.layers import GRU, Lambda
+from keras.layers import GRU, Lambda, Embedding, Concatenate
 from keras_preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 
-from nn.layers.Elmo import ElmoEmbeddingLayer
+from nn.layers.Elmo import ElmoEmbedding
 from nn.util.distances import exponent_neg_manhattan_distance
+from preprocessing.embeddings import prepare_embeddings
 
 
-def run_gru_elmo_benchmark(train_df, test_df, sent_cols, sim_col, validation_portion=0.1, n_hidden=100,
-                           embedding_dim=300,
-                           batch_size=64, n_epoch=500, optimizer=None, save_weights=None, load_weights=None,
-                           max_seq_length=None, model=None):
+def run_gru_benchmark(train_df, test_df, sent_cols, sim_col, validation_portion=0.1, n_hidden=100,
+                      batch_size=64, n_epoch=500, optimizer=None, save_weights=None, load_weights=None,
+                      max_seq_length=None, models=None):
     datasets = [train_df, test_df]
+
+    embeddings, embedding_dim = prepare_embeddings(datasets=datasets, question_cols=sent_cols,
+                                                   model=models[0])
 
     if max_seq_length is None:
         max_seq_length = max(train_df.sent_1.map(lambda x: len(x)).max(),
@@ -35,35 +38,43 @@ def run_gru_elmo_benchmark(train_df, test_df, sent_cols, sim_col, validation_por
     X_validation = {'left': X_validation.sent_1, 'right': X_validation.sent_2}
     X_test = {'left': test_df.sent_1, 'right': test_df.sent_2}
 
+    E_train = {'left': X_train['left'], 'right': X_train['right']}
+
     # Convert labels to their numpy representations
     Y_train = Y_train.values
     Y_validation = Y_validation.values
 
     # Zero padding
-    # for dataset, side in itertools.product([X_train, X_validation], ['left', 'right']):
-    #     dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
+    for dataset, side in itertools.product([X_train, X_validation], ['left', 'right']):
+        dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
 
-    # # The visible layer
-    # left_input = Input(shape=(max_seq_length,), dtype='int32')
-    # right_input = Input(shape=(max_seq_length,), dtype='int32')
+    # The visible layer
+    left_input_index_sequence = Input(shape=(max_seq_length,), dtype='int32', name="left_standard_input")
+    left_input_token_sequence = Input(shape=(1,), dtype="string", name="left_elmo_input")
 
-    left_input_text = Input(shape=(1,), dtype="string")
-    right_input_text = Input(shape=(1,), dtype="string")
+    right_input_index_sequence = Input(shape=(max_seq_length,), dtype='int32', name="right_standard_input")
+    right_input_token_sequence = Input(shape=(1,), dtype="string", name="left_elmo_input")
 
-    # embedding_layer = Embedding(len(embeddings), embedding_dim, weights=[embeddings], input_length=max_seq_length,
-    #                             trainable=False)
+    traditional_embedding_layer = Embedding(len(embeddings), embedding_dim, weights=[embeddings],
+                                            input_length=max_seq_length,
+                                            trainable=False)
 
-    embedding_layer = ElmoEmbeddingLayer(embedding_dim, input_length=1)
+    elmo_embedding_layer = Lambda(ElmoEmbedding, output_shape=(max_seq_length, 1024,))
 
     # Embedded version of the inputs
-    encoded_left = embedding_layer(left_input_text)
-    encoded_right = embedding_layer(right_input_text)
+    traditional_encoded_left = traditional_embedding_layer(left_input_index_sequence)
+    elmo_encoded_left = elmo_embedding_layer(left_input_token_sequence)
+
+    traditional_encoded_right = traditional_embedding_layer(right_input_index_sequence)
+    elmo_encoded_right = elmo_embedding_layer(right_input_token_sequence)
+
+    concatenate_layer = Concatenate()
+
+    encoded_left = concatenate_layer([traditional_encoded_left, elmo_encoded_left])
+    encoded_right = concatenate_layer([traditional_encoded_right, elmo_encoded_right])
 
     # Since this is a siamese network, both sides share the same LSTM
     shared_gru = GRU(n_hidden, name='gru')
-    # shared_gru = GRU(n_hidden, name='gru', return_sequences=True,
-    #                  kernel_initializer=glorot_normal(seed=12300),
-    #                  recurrent_initializer=orthogonal(gain=1.0, seed=10000))
 
     left_output = shared_gru(encoded_left)
     right_output = shared_gru(encoded_right)
@@ -73,7 +84,7 @@ def run_gru_elmo_benchmark(train_df, test_df, sent_cols, sim_col, validation_por
                             output_shape=lambda x: (x[0][0], 1))([left_output, right_output])
 
     # Pack it all up into a model
-    magru = Model([left_input_text, right_input_text], [magru_distance])
+    magru = Model([left_input, right_input], [magru_distance])
 
     optimizer = optimizer
 

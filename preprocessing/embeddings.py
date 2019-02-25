6f54f466e4b3002b80054a70a85ba67ca75094a2
TharinduDR <rhtdranasinghe@gmail.com>
2019-02-25 10:46:11 +0000

    019: adding elmo embedding

--- preprocessing/embeddings.py
+++ preprocessing/embeddings.py
@@ -100,6 +100,49 @@ def prepare_embeddings(model, datasets, question_cols):
     return embeddings, embedding_dim
 
 
+def prepare_embeddings_elmo(model, datasets, question_cols):
+
+    vocabulary = dict()
+    inverse_vocabulary = ['<unk>']
+    questions_cols = question_cols
+
+    # Iterate over the questions only of both training and test datasets
+    for dataset in datasets:
+        for index, row in dataset.iterrows():
+
+            # Iterate through the text of both questions of the row
+            for question in questions_cols:
+
+                q2n = []  # q2n -> question numbers representation
+                for word in text_to_word_list(row[question]):
+
+                    # Check for unwanted words
+                    if word not in model.vocab:
+                        continue
+
+                    if word not in vocabulary:
+                        vocabulary[word] = len(inverse_vocabulary)
+                        q2n.append(len(inverse_vocabulary))
+                        inverse_vocabulary.append(word)
+                    else:
+                        q2n.append(vocabulary[word])
+
+                # Replace questions as word to question as number representationindex, question, q2n
+                dataset.set_value(index, question, q2n)
+
+    embedding_dim = model.vector_size
+    embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)  # This will be the embedding matrix
+    embeddings[0] = 0  # So that the padding will be ignored
+
+    # Build the embedding matrix
+    for word, index in vocabulary.items():
+        if word in model.vocab:
+            embeddings[index] = model.word_vec(word)
+
+    return embeddings, embedding_dim
+
+
+
 def prepare_spanish_embeddings(model, datasets, question_cols):
     vocabulary = dict()
     inverse_vocabulary = ['<unk>']
